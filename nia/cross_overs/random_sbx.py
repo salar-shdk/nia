96be94efeb146534f2fc4592901d159d0217d382
salar-shdk <amir.shdk@yahoo.com>
2021-08-30 22:59:39 +0430

    add random sbx cross_over.

---
+++ nia/cross_overs/random_sbx.py
@@ -0,0 +1,20 @@
+from .cross_over import CrossOver
+import random
+import numpy as np
+
+class RandomSBX(CrossOver):
+    @CrossOver.initializer
+    def __init__(self, eta):
+        pass
+
+    def generate(self, population, num_children):
+        children = []
+        for i in range(int(num_children/2)):
+            father1, father2 = random.choices(population, k=2)
+            rand = random.random()
+            rand = 1 - rand if rand >= 0.5 else rand
+            beta = (2 * rand) ** (1 / (1 + self.eta))
+            son1 = 0.5 * (1 + beta) * father1 + (1 - beta) * father2
+            son2 = 0.5 * (1 - beta) * father1 + (1 + beta) * father2
+            children += [son1, son2]
+        return np.array(children)
